41cafc29e1ec53f4aa066f80035c88e04604f928
Andrii Vasyliev <sol@hiqdev.com>
2017-08-05 15:09:31 +0000

    disabled local view path

--- src/config/hidev.php
+++ src/config/hidev.php
@@ -1,12 +1,11 @@
 <?php
-
-/*
- * HiDev config for Python projects
+/**
+ * HiDev config for Python projects.
  *
  * @link      https://github.com/hiqdev/hidev-python
  * @package   hidev-python
  * @license   BSD-3-Clause
- * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ * @copyright Copyright (c) 2015-2017, HiQDev (http://hiqdev.com/)
  */
 
 return [
@@ -14,13 +13,13 @@ return [
         'include' => [
             __DIR__ . '/goals.yml',
         ],
-        'view' => [
+        /*'view' => [
             'theme' => [
                 'pathMap' => [
                     '@hidev/views' => '@hidev/python/views',
                 ],
             ],
-        ],
+        ],*/
         'vcsignore' => [
             '__pycache__/'  => 'Byte-compiled / optimized / DLL files',
             '*.py[cod]'     => 'Byte-compiled / optimized / DLL files',
