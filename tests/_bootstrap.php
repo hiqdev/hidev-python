5fe9d2994a2f5a066f7c5657e3dd8f42934efdc1
Andrii Vasyliev <sol@hiqdev.com>
2017-08-05 15:09:42 +0000

    csfixed

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,12 +1,11 @@
 <?php
-
-/*
- * HiDev config for Python projects
+/**
+ * HiDev config for Python projects.
  *
  * @link      https://github.com/hiqdev/hidev-python
  * @package   hidev-python
  * @license   BSD-3-Clause
- * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ * @copyright Copyright (c) 2015-2017, HiQDev (http://hiqdev.com/)
  */
 
 error_reporting(E_ALL & ~E_NOTICE);
